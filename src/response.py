97a0b5935479a0ff493123e2a7a1c8ef0f73737f
Javierko <33197433+Javierko@users.noreply.github.com>
2024-06-29 00:41:41 +0200

    feat: response util

---
+++ src/response.py
@@ -0,0 +1,9 @@
+from typing import Any
+
+
+def response(_type: str, data: dict[Any, Any]) -> dict[Any, Any]:
+    return {**data, "type": _type}
+
+
+def error_response(message: str) -> dict[Any, Any]:
+    return response("error", {"message": message})
