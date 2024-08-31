066c4553900576c6daaff1333d473b62dc7afb5c
Javierko <33197433+Javierko@users.noreply.github.com>
2024-08-31 16:27:51 +0200

    feat: prepare eyetracker abstraction

---
+++ DevelexBridge/Bridge/Enums/EyeTrackerState.cs
@@ -0,0 +1,10 @@
+﻿namespace Bridge.Enums;
+
+public enum EyeTrackerState
+{
+    Disconnected,
+    Connecting,
+    Connected,
+    Stopped,
+    Started,
+}
---
+++ DevelexBridge/Bridge/Models/EyeTracker.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Forms.VisualStyles;
+using Bridge.Enums;
+
+namespace Bridge.Models;
+
+public abstract class EyeTracker
+{
+    public abstract EyeTrackerState State { get; set; }
+    
+    public abstract void Connect();
+    public abstract void Start();
+    public abstract void Stop();
+    public abstract void Calibrate();
+    public abstract void Disconnect();
+}
