4d2cbd1f41553d68616afb6271b4d2d9fe0c0bd5
Javierko <33197433+Javierko@users.noreply.github.com>
2024-12-01 23:02:37 +0100

    feat: double int parsing, dictionary get

--- DevelexBridge/Bridge/Extensions/DictionaryExtensions.cs
+++ DevelexBridge/Bridge/Extensions/DictionaryExtensions.cs
@@ -1,6 +1,14 @@
 ﻿namespace Bridge.Extensions;
 
-public class DictionaryExtensions
+public static class DictionaryExtensions
 {
-    
+    public static TV Get<TK, TV>(this Dictionary<TK, TV> dictionary, TK key, TV defaultValue = default(TV)) where TK : notnull
+    {
+        if (dictionary.TryGetValue(key, out var value))
+        {
+            return value ?? defaultValue;
+        }
+
+        return defaultValue;
+    }
 }
--- DevelexBridge/Bridge/Extensions/ParseExtensions.cs
+++ DevelexBridge/Bridge/Extensions/ParseExtensions.cs
@@ -1,6 +1,16 @@
-﻿namespace Bridge.Extensions;
+﻿using System.Globalization;
 
-public class ParseExtensions
+namespace Bridge.Extensions;
+
+public static class ParseExtensions
 {
-    
+    public static double ParseDouble(this string input)
+    {
+        return double.TryParse(input, out var result) ? result : 0;
+    }
+
+    public static int ParseInt(this string input)
+    {
+        return int.TryParse(input, out var result) ? result : 0;
+    }
 }
