53cf18534851f0e7b6ae513283bde931f80a718a
Javierko <33197433+Javierko@users.noreply.github.com>
2025-01-11 21:05:54 +0100

    feat: eyelogic dlls

--- DevelexBridge/Bridge/Program.cs
+++ DevelexBridge/Bridge/Program.cs
@@ -1,15 +1,22 @@
+using System.Runtime.InteropServices;
+
 namespace Bridge;
 
 static class Program
 {
+    [DllImport("kernel32", SetLastError = true, CharSet = CharSet.Auto)]
+    private static extern bool SetDllDirectory(string lpPathName);
+    
     /// <summary>
     ///  The main entry point for the application.
     /// </summary>
     [STAThread]
     static void Main()
     {
-        // To customize application configuration such as set high DPI settings or default font,
-        // see https://aka.ms/applicationconfiguration.
+        // TODO: for future eyetrackers do some kind of merging dll
+        var dllPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "EyeTrackers", "EyeLogic", "Libs");
+        SetDllDirectory(dllPath);
+        
         ApplicationConfiguration.Initialize();
         Application.Run(new BridgeWindow());
     }
