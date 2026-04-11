7a497577c0aea97ba2fa04df93b2ae40edcd04a8
František Holubec <frantisek.holubec@gmail.com>
2026-04-11 22:32:58 +0200

    feat: DateTimeExtensions now uses higher frequency stopwatch

--- DevelexBridge/Bridge/Extensions/DateTimeExtensions.cs
+++ DevelexBridge/Bridge/Extensions/DateTimeExtensions.cs
@@ -1,10 +1,24 @@
-﻿using System.Globalization;
+﻿using System.Diagnostics;
+using System.Globalization;
 
 namespace Bridge.Extensions;
 
 public static class DateTimeExtensions
 {
-    public static string IsoNow => DateTime.UtcNow.ToString("o", CultureInfo.InvariantCulture);
+    private static readonly DateTime ANCHOR_UTC = DateTime.UtcNow;
+    private static readonly long ANCHOR_TICKS = Stopwatch.GetTimestamp();
+    private static readonly double TICK_FREQUENCY = Stopwatch.Frequency;
+
+    public static string IsoNow => HighResUtcNow.ToString("o", CultureInfo.InvariantCulture);
+
+    public static DateTime HighResUtcNow
+    {
+        get
+        {
+            var elapsed = (Stopwatch.GetTimestamp() - ANCHOR_TICKS) / TICK_FREQUENCY;
+            return ANCHOR_UTC.AddSeconds(elapsed);
+        }
+    }
 
     public static string ToIso(this DateTime dateTime)
     {
